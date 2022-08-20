af5f92d0ec9ab4f1122e13c63b822c817cf59bc9
Nurlan Mehdiyev <nurlan.mehdiyev.resume@gmail.com>
2022-08-20 16:25:02 +0400

    fix

--- arrays.js
+++ arrays.js
@@ -1,10 +1,13 @@
-'use string';
-let myArray = ['001', '002', '003'];
-myArray = myArray.map((item) => {
-    return `<div>${item}</div>`
-})
-document.getElementById("arrays").innerHTML = myArray.join('');
+addAll();
+
 function ArrayPopMethod() {
     myArray.pop();
+    document.getElementById("arrays").innerHTML = myArray.join('');
+}
+function addAll() {
+    myArray = ['001', '002', '003'];
+    myArray = myArray.map((item) => {
+        return `<div>${item}</div>`
+    })
     document.getElementById("arrays").innerHTML = myArray.join('');
 }
