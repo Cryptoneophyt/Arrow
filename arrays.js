b09aa58716602bf245cac26f8891ca732293e201
Artem Khan <107679569+Cryptoneophyt@users.noreply.github.com>
2022-08-20 17:16:36 +0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,15 @@
+'use string';
+// function showArray() {
+let myArray = ['001', '002', '003'];
+//     myArray = myArray.map((item) => {
+//         return `<div>${item}</div>`
+//     })
+//     document.getElementById("arrays").innerHTML = myArray.join('');
+// }
+// showArray();
+let button = document.querySelector('.button');
+button.onclick = function () {
+    // myArray.pop();
+    // console.log(myArray);
+    console.log('Клик!');
+};
